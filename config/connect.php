80d9e2dc8b4968655fb0c68bc3ef943696d232a9
StenQuidelleur <sten.quidelleur@outlook.fr>
2023-03-16 07:50:20 +0100

    Cours PDO clean

--- config/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("DSN", "mysql:host=localhost:8889;dbname=pdo_cours");
-define("USER", "root");
-define("PASS", "root");
-define("DB_NAME", "pdo_cours");
-define("DB_DUMP_PATH", "./pdo_cours.sql");
